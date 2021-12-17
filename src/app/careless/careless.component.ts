5fb0a9148a8a585a8289c62f007c2b5e4b0ab275
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-12-17 22:19:35 +0300

    removed unnecessary console logs

--- src/app/careless/careless.component.ts
+++ src/app/careless/careless.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 import { IntervalService } from '../services/interval/interval.service';
+import { UnsubscribeService } from '../services/unsubscribe/unsubscribe.service';
 
 @Component({
   selector: 'app-careless',
   template: `
     <p>ಠ_ಠ</p>
-  `,
+    `,
+    providers: [UnsubscribeService],
 })
 export class CarelessComponent implements OnInit {
-  public constructor(private intervalService: IntervalService) {}
+  private permutations: (string | string[])[] = [];
+  public constructor(private intervalService: IntervalService, private unsubscribeService: UnsubscribeService) {}
 
   public ngOnInit(): void {
-    this.intervalService.getInterval.subscribe(async (i) => {
-      console.log(i);
-      console.log(findPermutations('abcdefg'));
+    this.intervalService.getInterval.pipe(this.unsubscribeService.untilDestroyed).subscribe(async () => {
+      this.permutations.push(findPermutations('abcdefg'));
     });
   }
 }
