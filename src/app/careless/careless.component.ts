0164e84c0df5a8db3c75cfb79d31c4e198e32133
Volodymyr Yepishev <i.m.bwca@gmail.com>
2021-12-18 12:04:48 +0300

    threw out permutations

--- src/app/careless/careless.component.ts
+++ src/app/careless/careless.component.ts
@@ -5,37 +5,16 @@ import { UnsubscribeService } from '../services/unsubscribe/unsubscribe.service'
 
 @Component({
   selector: 'app-careless',
-  template: `
-    <p>ಠ_ಠ</p>
-    `,
-    providers: [UnsubscribeService],
+  template: `<p>ಠ_ಠ</p>`,
+  providers: [UnsubscribeService],
 })
 export class CarelessComponent implements OnInit {
-  private permutations: (string | string[])[] = [];
+  private garbage: string[][] = [];
   public constructor(private intervalService: IntervalService, private unsubscribeService: UnsubscribeService) {}
 
   public ngOnInit(): void {
     this.intervalService.getInterval.pipe(this.unsubscribeService.untilDestroyed).subscribe(async () => {
-      this.permutations.push(findPermutations('abcdefg'));
+      this.garbage.push(Array(5000).fill("some trash"));
     });
   }
-}
-
-function findPermutations(str: string): string[] | string {
-  if (str.length < 2) {
-    return str;
-  }
-
-  const permutations: string[] = [];
-
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-
-    const remainingChars = str.slice(0, i) + str.slice(i + 1, str.length);
-
-    for (const permutation of findPermutations(remainingChars)) {
-      permutations.push(char + permutation);
-    }
-  }
-  return permutations;
 }
